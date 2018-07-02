bb36952a66e3ad97865cd3fb6b024d81a4148221
NMroth42 <mannoroth42@gmail.com>
2018-07-02 17:53:48 +0700

    add register

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
 console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `hello ${req.body.email}!`
+  })
+})
+
+app.listen(process.env.PORT || 8081)
