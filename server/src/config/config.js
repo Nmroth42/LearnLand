f1b35f948d30854c777aac25e821168052fc4170
NMroth42 <mannoroth42@gmail.com>
2018-07-03 16:23:48 +0700

    add routes

---
+++ server/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'learnland',
+    user: process.env.DB_USER || 'learnland',
+    password: process.env.DB_PASS || 'learnland',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './learnland.sqlite'
+
+    }
+  }
+}
